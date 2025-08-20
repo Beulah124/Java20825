b1682f90332670671238fa2279f61c28b5a94c79
Beulah124 <sekhar.se@cmrtc.ac.in>
2025-08-20 13:14:32 +0530

    java program small chane

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello{
     public static void main(String args[]){
-        System.out.println("Welcome to Jenkins World");
+        System.out.println("Welcome to Jenkins World and  we are 3rd cse E section");
     }
 }
